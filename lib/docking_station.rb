11ad22b2ad9a828c23c17f3338a34af5ca1d73b4
Antonia Cronin <antoniajcronin@gmail.com>
2018-03-20 11:35:08 +0000

    dock bike tests@

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,15 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
-    bike = Bike.new
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+
+  def dock_bike(bike)
+    fail 'Docking station full' if @bike
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,40 @@ require 'docking_station'
 describe DockingStation do
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
-    # it { is_expected.to eq bike }
 
-    it 'returns a working bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'bike is working' do
+      bike = Bike.new
+      subject.dock_bike(bike)
       bike = subject.release_bike
       expect(bike).to be_working
     end
+
+    it 'raises an error if no bikes are available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
   end
+
+  describe '#dock_bike' do
+    it { is_expected.to respond_to :dock_bike }
+
+    it 'allows a bike to be docked' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bike).to eq bike
+    end
+
+    it 'raises an error if there is already a bike docked' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect { subject.dock_bike(bike) }.to raise_error 'Docking station full'
+    end
+  end
+
 end
